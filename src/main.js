302f03525c230dd4c3e230bddac45214d7d0046b
dbfjru <dbfjru@gmail.com>
2025-07-08 21:50:39 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -30,3 +30,15 @@ document.addEventListener('scroll', () => {
     arrow.classList.remove('arrow-up--visible');
   }
 })
+
+//Navbar 토글 버튼 클릭 처리
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+})
+
+// Navbar 메뉴 클릭시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
+})
